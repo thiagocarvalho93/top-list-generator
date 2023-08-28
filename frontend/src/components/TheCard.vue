e728cf6453524effbecb6424eb86efae2c88c090
thiagocarvalho93 <thiagopaes1993@gmail.com>
2023-08-27 22:45:33 -0300

    ✨ Adicionando listas (mockado)

--- frontend/src/components/TheCard.vue
+++ frontend/src/components/TheCard.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link class="text-center" :to="to">
     <div
-      class="block max-w-sm p-6 bg-white shadow rounded-lg border-black hover:bg-slate-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      class="block max-w-sm h-full align-center p-6 bg-white shadow rounded-lg border-black hover:bg-slate-200 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
       <p class="font-semibold text-gray-700 dark:text-gray-400">
         {{ descricao }}
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="home">
-    <div class="flex flex-col justify-between sm:flex-row">
-      <h1 class="text-2xl font-semibold mb-6">Listas</h1>
-    </div>
-    <div>
-      <button
-        class="mb-6 block max-w-sm p-3 rounded-lg bg-purple-600 text-white font-bold border-black shadow hover:bg-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-        Adicionar
-      </button>
+    <h1 class="text-2xl font-semibold mb-6">Lists</h1>
+    <div class="flex mb-6">
+      <div class="mr-4">
+        <input
+          v-model="nomeNovaLista"
+          type="text"
+          placeholder="New list name"
+          id="small-input"
+          class="block p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div>
+        <button
+          class="block max-w-sm p-2 rounded-lg bg-purple-600 text-white sm:text-xs font-bold border-black shadow hover:bg-purple-700 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          @click="adicionar"
+        >
+          Add
+        </button>
+      </div>
     </div>
     <div class="grid grid-cols-1 md:grid-cols-6 gap-4">
       <the-card
@@ -22,15 +32,23 @@
 </template>
 
 <script>
-import listas from "@/mocks/listas.json";
+import listasJson from "@/mocks/listas.json";
 import TheCard from "@/components/TheCard.vue";
 
 export default {
+  components: { TheCard },
   data() {
     return {
-      listas,
+      listas: listasJson,
+      nomeNovaLista: "",
     };
   },
-  components: { TheCard },
+  methods: {
+    adicionar() {
+      let ultimoId = Math.max(...this.listas.map((x) => x.id));
+      this.listas.push({ id: ultimoId + 1, descricao: this.nomeNovaLista });
+    },
+  },
+  created() {},
 };
 </script>
